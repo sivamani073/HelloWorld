3123d04c67d7c95dda0120677a3b58c431892e4f
mathi-ammu <gandhimathivenkatesan05@gmail.com>
2019-06-13 13:56:13 +0530

    hello world for loop program

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,11 @@ class HelloWorld
 { 
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World"); 
-    } 
-} 
+    
+	public static void main(String[] args) {  
+    //Code of Java for loop  
+    for(int i=1;i<=10;i++){  
+        System.out.println("hello world " +i);  
+    }  
+}  
+}  
